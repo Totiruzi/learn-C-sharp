22aea01afab1ffbe851cbcb8a5cc525e20ae7716
Zhang Han <463808243@qq.com>
2018-10-20 22:24:05 +0800

    Design Patterns initial commit

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    //参考《剑指offer》
+    //利用静态构造函数，当 .NET运行时发现第一次使用一个类型的，就自动调用该类型的静态构造函数
+    // c#的语法保证静态构造函数只调用一次
+    public sealed class Singleton_1
+    {
+        private Singleton_1() { }
+        private static Singleton_1 instance = new Singleton_1();
+        public static Singleton_1 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
