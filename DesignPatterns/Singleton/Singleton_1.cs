12d40b166d24321071ca6a888d7a30ebf3625878
Zhang Han <463808243@qq.com>
2018-10-22 20:02:01 +0800

    update singleton

--- DesignPatterns/Singleton/Singleton_1.cs
+++ DesignPatterns/Singleton/Singleton_1.cs
@@ -4,11 +4,13 @@ namespace Singleton
 {
     //参考《剑指offer》
     //利用静态构造函数，当 .NET运行时发现第一次使用一个类型的，就自动调用该类型的静态构造函数
+    // 缺点：会过早的创建一个实例
     // c#的语法保证静态构造函数只调用一次
     public sealed class Singleton_1
     {
         private Singleton_1() { }
         private static Singleton_1 instance = new Singleton_1();
+        // instance封装在Instance里，通过get和set关键字控制可读还是可写
         public static Singleton_1 Instance
         {
             get
---
+++ DesignPatterns/Singleton/Singleton_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    // 实现按需创建实例
+    public sealed class Singleton_2
+    {
+        Singleton_2() {}
+        public static Singleton_2 Instance
+        {
+
+        }
+    }
+}
