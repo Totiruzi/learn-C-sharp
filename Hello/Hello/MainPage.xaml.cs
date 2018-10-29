4e50a6d6afe93ee901f2a350de0605f96062ac1a
Zhang Han <463808243@qq.com>
2018-10-29 19:46:07 +0800

    update

--- Hello/Hello/MainPage.xaml.cs
+++ Hello/Hello/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Hello
         {
             this.InitializeComponent();
         }
+
+        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
