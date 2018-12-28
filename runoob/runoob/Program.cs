d87407465807c454fba06390e83cb99c81baf218
Zhang Han <463808243@qq.com>
2018-12-28 23:13:45 +0800

    array

--- runoob/runoob/Program.cs
+++ runoob/runoob/Program.cs
@@ -10,6 +10,13 @@ namespace runoob
     {
         static void Main(string[] args)
         {
+            int n = 10;
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+                nums[i] = i * 2;
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("num[{0}] = {1}", i, nums[i]);
+            Console.ReadKey();   
         }
     }
 }
