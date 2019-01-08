93e9395a67533075a6e74208968f4c63c142ab50
Zhang Han <463808243@qq.com>
2019-01-08 20:37:12 +0800

    polymorphism

--- runoob/runoob/Inheritance.cs
+++ runoob/runoob/Inheritance.cs
@@ -11,57 +11,62 @@ namespace runoob
      * override是重写，即将基类的方法在派生类里直接抹去重新写，故而调用的方法就是子类方法；而new只是将基类的方法在派生类里隐藏起来，故而调用的仍旧是基类方法。
      */
 
-    /// <summary>  
-    /// 父类  
-    /// </summary>  
-    public class ParentClass
+    class ParentClass1
     {
         public virtual void ParVirMethod()
         {
-            Console.WriteLine("父类的方法...");
+            Console.WriteLine("parent1 virtual");
         }
     }
 
-    /// <summary>  
-    /// 子类0 
-    /// </summary>  
-    public class ChildClass0 : ParentClass
+    abstract class ParentClass2
     {
-        public void ParVirMethod()
-        {
-            Console.WriteLine("子类0的方法...");
-        }
-
-        public void Test()
-        {
-            Console.WriteLine("子类0的其他方法...");
-        }
+        // 抽象函数只能在抽象类中定义，并且没有函数体
+        public abstract void ParAbsMethod();
     }
 
-    /// <summary>  
-    /// 子类1  
-    /// </summary>  
-    public class ChildClass1 : ParentClass
+    class ChildClass1 : ParentClass1
     {
         public override void ParVirMethod()
         {
-            Console.WriteLine("子类1的方法...");
+            Console.WriteLine("class1 override virtual");
         }
     }
-
-    /// <summary>  
-    /// 子类2  
-    /// </summary>  
-    public class ChildClass2 : ParentClass
+ 
+    class ChildClass2 : ParentClass1
     {
         public new void ParVirMethod()
         {
-            Console.WriteLine("子类2的方法...");
+            Console.WriteLine("class2 new");
         }
-
+    }
+    class ChildClass3 : ParentClass2
+    {
+        public override void ParAbsMethod()
+        {
+            Console.WriteLine("class3 override abstract");
+        }
+    }
+    class ChildTest
+    {
+        //---- 隐藏方法的调用----
         public void Test()
         {
-            Console.WriteLine("子类2的其他方法...");
+            //---隐藏方法调用---
+            //用什么类型来声明，在调用方法的时候就会调用什么类型的方法。
+            ParentClass1 par1 = new ChildClass2();
+            ChildClass2 child = new ChildClass2();
+            par1.ParVirMethod();
+            child.ParVirMethod();
+
+            //---虚方法的调用---
+            //用什么类型来实例化，在调用方法的时候就会调用什么类型的方法。
+            par1 = new ChildClass1();
+            par1.ParVirMethod();
+            ParentClass2 par2 = new ChildClass3();
+            par2.ParAbsMethod();
+
         }
+          
     }
 }
--- runoob/runoob/Main.cs
+++ runoob/runoob/Main.cs
@@ -10,14 +10,8 @@ namespace runoob
     {
         static void Main(string[] args)
         {
-            ParentClass par = new ChildClass0();
-            par.ParVirMethod(); //结果："子类0的方法"
-
-            par = new ChildClass1();
-            par.ParVirMethod(); //结果："子类1的方法"，调用子类的方法，实现了多态
-
-            par = new ChildClass2();
-            par.ParVirMethod(); //结果："父类的方法"，调用父类的方法，没有实现多态  
+            ChildTest t = new ChildTest();
+            t.Test();
             Console.ReadKey();
         }
     }
