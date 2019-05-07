116a807794b9055db131e097c38ae9dde59a046b
Zhang Han <463808243@qq.com>
2019-05-07 23:04:26 +0800

    Update Attribute.cs

--- runoob/runoob/Attribute.cs
+++ runoob/runoob/Attribute.cs
@@ -17,5 +17,26 @@ namespace runoob
             Console.ReadKey();
         }
     }
+
+    // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/attributes/index
+    //creating custom Attributes
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
+    public class Author : System.Attribute
+    {
+        private string name;
+        public double version;
+
+        public Author(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+    }
+
+    [Author("Han", version = 1.1)]
+    class AuthorClass
+    {
+        // Han's code goes here...
+    }
 }
 
