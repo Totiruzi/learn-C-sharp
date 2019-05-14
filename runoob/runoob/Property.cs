493de693e620f02ae7d87681c861e36019acbb98
Zhang Han <463808243@qq.com>
2019-05-14 22:53:29 +0800

    Create Property.cs

---
+++ runoob/runoob/Property.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runoob
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person(string first, string last)
+        {
+            _firstName = first;
+            _lastName = last;
+        }
+        //  the read-only Name property as an expression-bodied member.
+        public string Name => $"{_firstName} {_lastName}";
+    }
+}
