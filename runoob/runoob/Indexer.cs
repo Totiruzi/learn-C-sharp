000d95b2a67e99d4bf69dd3dc915a704ab1fd95d
Zhang Han <463808243@qq.com>
2019-05-18 18:37:33 +0800

    update Indexer.cs

--- runoob/runoob/Indexer.cs
+++ runoob/runoob/Indexer.cs
@@ -16,11 +16,29 @@ namespace runoob
             get { return arr[i]; }
             set { arr[i] = value; }
         }
+        
+    }
+
+    //Starting with C# 7.0, both the get and set accessor can be an implemented as expression-bodied members. 
+    //In this case, both get and set keywords must be used.
+    class SampleCollectionExpression<T>
+    {
+        private T[] arr = new T[100];
+        public T this[int i]
+        {
+            get => arr[i];
+            set => arr[i] = value;
+        }
+    }
+    class IndexerTest
+    {
         public static void Test()
         {
             SampleCollection<String> stringCollection = new SampleCollection<string>();
-            stringCollection[0] = "hello world";
-            System.Console.WriteLine(stringCollection[0]);
+            var stringCollectionExp = new SampleCollectionExpression<string>();
+            stringCollection[0] = "hello";
+            stringCollectionExp[0] = "world";
+            System.Console.WriteLine(stringCollection[0] + " " + stringCollectionExp[0]);
         }
     }
 }
